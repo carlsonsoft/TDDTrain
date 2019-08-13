14b1fc8c7248b5706a9ec89022ab6e678801a26f
carlson <cui18325787035@gmail.com>
2019-08-13 19:32:17 +0800

    第二次测试 用时15分钟

---
+++ FizzBuzz/SecTrain/SecFizzBuzz/GameNumber/FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GameNumber
+{
+    public class FizzBuzz
+    {
+        public static string At(int position)
+        {
+            var result = "";
+            if (position % 3 == 0 || position.ToString().Contains("3"))
+            {
+                result += "Fizz";
+            }
+            if (position % 5 == 0 || position.ToString().Contains("5"))
+            {
+                result += "Buzz";
+            }
+
+            if (result == "")
+            {
+                result += position;
+            }
+            return result;
+        }
+
+        private static bool IsDividedBy(int position, int divided)
+        {
+            return position % divided == 0;
+        }
+    }
+}
